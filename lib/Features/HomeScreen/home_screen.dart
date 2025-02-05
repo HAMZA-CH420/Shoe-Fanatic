ef605abd14f9c8dcdb73f7bbe9c92955d7faa5d6
HAMZA-CH <hakram554r@gmail.com>
2025-02-05 11:46:59 +0500

    minor changes

--- lib/Features/HomeScreen/home_screen.dart
+++ lib/Features/HomeScreen/home_screen.dart
@@ -7,8 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("HomeScreen"),
-        centerTitle: true,
+        title: Text("Home screen"),
       ),
       body: Center(
         child: Text("Hi, Welcome Aboard"),
