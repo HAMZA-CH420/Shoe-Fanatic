db377cbac91a7f612c8cce479129213917d1f17c
HAMZA-CH <hakram554r@gmail.com>
2025-02-18 12:39:02 +0500

    minor changes

---
+++ lib/Features/SellingScreen/TabScreens/myshoes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyShoes extends StatelessWidget {
+  const MyShoes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("MyShoes"));
+  }
+}
--- lib/Features/SellingScreen/selling_screen.dart
+++ lib/Features/SellingScreen/selling_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shoe_fantastic/Features/SellingScreen/TabScreens/home.dart';
+import 'package:shoe_fantastic/Features/SellingScreen/TabScreens/myshoes.dart';
+import 'package:shoe_fantastic/Features/SellingScreen/TabScreens/sold.dart';
 import 'package:shoe_fantastic/Ui%20Helper/Color%20Palate/color_palate.dart';
 
 class SellingScreen extends StatefulWidget {
@@ -15,70 +19,82 @@ class _SellingScreenState extends State<SellingScreen> {
     "My Shoes",
     "Sold",
   ];
+  final List<Widget> screens = [
+    Home(),
+    MyShoes(),
+    Sold(),
+  ];
   int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Selling",
-          style: GoogleFonts.publicSans(
-              fontWeight: FontWeight.w500,
-              fontSize: 20,
-              color: Palate.blackColor),
-        ),
-        centerTitle: true,
-        leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+      appBar: appBar(size),
+      body: IndexedStack(
+        index: currentIndex,
+        children: screens,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: SizedBox(
-          height: double.infinity,
-          width: double.infinity,
-          child: Column(
-            children: [
-              SizedBox(
-                width: double.infinity,
-                height: 40,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.symmetric(horizontal: 7),
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: tabBarTitles.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          currentIndex = index;
-                        });
-                      },
-                      child: AnimatedContainer(
-                        duration: const Duration(milliseconds: 200),
-                        width: size.width / 3.7,
-                        margin: const EdgeInsets.only(left: 5),
-                        decoration: BoxDecoration(
-                            color: currentIndex == index
-                                ? Palate.primaryColor
-                                : Color(0xffD9D9D9),
-                            borderRadius: BorderRadius.circular(25)),
-                        alignment: Alignment.center,
-                        child: Text(
-                          tabBarTitles[index],
-                          style: GoogleFonts.publicSans(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                            color: currentIndex == index
-                                ? Colors.white
-                                : Colors.black38,
-                          ),
-                        ),
-                      ),
-                    );
+    );
+  }
+
+  /// Custom Appbar
+  AppBar appBar(var size) {
+    return AppBar(
+      scrolledUnderElevation: 0.0,
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      systemOverlayStyle: SystemUiOverlayStyle.dark,
+      title: Text(
+        "Selling",
+        style: GoogleFonts.publicSans(
+            fontWeight: FontWeight.w500,
+            fontSize: 20,
+            color: Palate.blackColor),
+      ),
+      centerTitle: true,
+      leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+      bottom: PreferredSize(
+        preferredSize: Size.fromHeight(65),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+          child: SizedBox(
+            width: double.infinity,
+            height: 40,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: tabBarTitles.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      currentIndex = index;
+                    });
                   },
-                ),
-              ),
-            ],
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 200),
+                    width: size.width / 3.7,
+                    margin: const EdgeInsets.only(left: 5),
+                    decoration: BoxDecoration(
+                        color: currentIndex == index
+                            ? Palate.primaryColor
+                            : Color(0xffD9D9D9),
+                        borderRadius: BorderRadius.circular(25)),
+                    alignment: Alignment.center,
+                    child: Text(
+                      tabBarTitles[index],
+                      style: GoogleFonts.publicSans(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: currentIndex == index
+                            ? Colors.white
+                            : Colors.black38,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
         ),
       ),
