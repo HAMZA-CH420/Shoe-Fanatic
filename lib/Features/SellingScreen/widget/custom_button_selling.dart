d816beb87300fd01ea0a71d3f55ecb33d3a691b3
HAMZA-CH <hakram554r@gmail.com>
2025-02-19 22:12:37 +0500

    minor changes

--- lib/Features/SellingScreen/widget/custom_button_selling.dart
+++ lib/Features/SellingScreen/widget/custom_button_selling.dart
@@ -13,43 +13,44 @@ class CustomButtonSelling extends StatelessWidget {
   final VoidCallback onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 17),
-      margin: const EdgeInsets.only(top: 20),
-      height: 48,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(25),
-        color: Colors.white,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            spacing: 11,
-            children: [
-              Icon(
-                icon,
-                color: Palate.blackColor,
-                size: 19,
-              ),
-              Text(
-                btnName,
-                style: GoogleFonts.publicSans(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 16,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 17),
+        margin: const EdgeInsets.only(top: 20),
+        height: 48,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          color: Colors.white,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              spacing: 11,
+              children: [
+                Icon(
+                  icon,
+                  color: Palate.blackColor,
+                  size: 19,
                 ),
-              )
-            ],
-          ),
-          InkWell(
-              onTap: onTap,
-              child: Icon(
-                Icons.arrow_forward_ios,
-                size: 15,
-                color: Palate.blackColor,
-              ))
-        ],
+                Text(
+                  btnName,
+                  style: GoogleFonts.publicSans(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16,
+                  ),
+                )
+              ],
+            ),
+            Icon(
+              Icons.arrow_forward_ios,
+              size: 15,
+              color: Palate.blackColor,
+            )
+          ],
+        ),
       ),
     );
   }
