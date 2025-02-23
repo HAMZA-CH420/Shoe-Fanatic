ba28124fcd364bc32391b2660615605af388ab1b
HAMZA-CH <hakram554r@gmail.com>
2025-02-23 10:27:36 +0500

    minor changes

--- lib/Features/HomeScreen/Widgets/fav_button.dart
+++ lib/Features/HomeScreen/Widgets/fav_button.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shoe_fantastic/Ui%20Helper/Color%20Palate/color_palate.dart';
 
 class FavButton extends StatefulWidget {
   const FavButton({super.key, required this.isRecommended});
@@ -17,6 +21,7 @@ class _FavButtonState extends State<FavButton> {
       child: GestureDetector(
           onTap: () {
             setState(() {
+              isFav ? null : _showAlertDialogue(context);
               isFav = !isFav;
             });
           },
@@ -30,5 +35,83 @@ class _FavButtonState extends State<FavButton> {
                     : Colors.white,
           )),
     );
+  }
+
+  Future<void> _showAlertDialogue(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Column(
+          spacing: 10,
+          children: [
+            SvgPicture.asset("assets/images/fav-image.svg"),
+            Text(
+              'Favorites added!',
+              style: GoogleFonts.poppins(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                color: Palate.blackColor,
+              ),
+            ),
+            SizedBox(
+              width: 200,
+              height: 50,
+              child: Text(
+                  'By visiting your favorites, you can see everything you like',
+                  style: GoogleFonts.poppins(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w500,
+                    color: Palate.blackColor,
+                  )),
+            ),
+            AlertDialogueButton(
+              onTap: () async {
+                final pref = await SharedPreferences.getInstance();
+                pref.setBool("isFav", false);
+                Navigator.pop(context);
+              },
+              btnName: "Got it",
+              isSecondButton: false,
+            ),
+            AlertDialogueButton(
+              onTap: () {},
+              btnName: "Go to Favorites",
+              isSecondButton: true,
+            ),
+          ],
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
+
+class AlertDialogueButton extends StatelessWidget {
+  const AlertDialogueButton(
+      {super.key,
+      required this.btnName,
+      required this.isSecondButton,
+      required this.onTap});
+  final String btnName;
+  final bool isSecondButton;
+  final VoidCallback onTap;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          fixedSize: Size(200, 40),
+          backgroundColor: isSecondButton ? Colors.white : Palate.primaryColor,
+        ),
+        child: Text(
+          btnName,
+          style: GoogleFonts.publicSans(
+            fontWeight: FontWeight.w600,
+            fontSize: 16,
+            color: isSecondButton ? Palate.blackColor : Colors.white,
+          ),
+        ));
   }
 }
