85d82928abd16b0b41117ce8a6d7217f4df8a5fb
HAMZA-CH <hakram554r@gmail.com>
2025-03-07 15:41:43 +0500

    minor changes

--- lib/Features/Terms & Conditions/terms_conditions.dart
+++ lib/Features/Terms & Conditions/terms_conditions.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shoe_fantastic/Ui%20Helper/Color%20Palate/color_palate.dart';
 
 import '../PrivacyPolicy Screen/Widgets/custom_banner.dart';
 
-class TermsConditions extends StatelessWidget {
+class TermsConditions extends StatefulWidget {
   const TermsConditions({super.key});
+
+  @override
+  State<TermsConditions> createState() => _TermsConditionsState();
+}
+
+class _TermsConditionsState extends State<TermsConditions> {
+  bool isTermsSelected = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Privacy Policy",
+        title: Text("Terms & Conditions",
             style: GoogleFonts.publicSans(
               fontWeight: FontWeight.w600,
               fontSize: 18,
@@ -29,7 +37,7 @@ class TermsConditions extends StatelessWidget {
           spacing: 15,
           children: [
             CustomBannerPolicy(
-              bannerName: "Privacy Policy of Shoe Fanatic",
+              bannerName: "Terms & Conditions of Shoe Fanatic",
             ),
             Text(
               "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years.",
@@ -40,6 +48,46 @@ class TermsConditions extends StatelessWidget {
               " web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web site.",
               style: GoogleFonts.publicSans(
                   fontWeight: FontWeight.w400, fontSize: 15),
+            ),
+            Row(
+              children: [
+                Radio.adaptive(
+                  toggleable: true,
+                  activeColor: Palate.primaryColor,
+                  value: true,
+                  groupValue: isTermsSelected,
+                  onChanged: (value) {
+                    setState(() {
+                      isTermsSelected = !isTermsSelected;
+                    });
+                  },
+                ),
+                Text(
+                  "I agree with the ",
+                  style: GoogleFonts.publicSans(
+                      fontWeight: FontWeight.w600,
+                      color: Palate.blackColor,
+                      fontSize: 15),
+                ),
+                Text(
+                  "Terms and Conditions",
+                  style: GoogleFonts.publicSans(
+                      fontWeight: FontWeight.w600,
+                      color: Palate.primaryColor,
+                      fontSize: 15),
+                ),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height / 8,
+                ),
+                Container(
+                  height: 40,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Palate.primaryColor,
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
