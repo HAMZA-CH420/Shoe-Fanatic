ca9ddb358e189fca7368810fce3b9828ca1b01ac
HAMZA-CH <hakram554r@gmail.com>
2025-03-08 08:33:01 +0500

    minor changes

--- lib/Features/DrawerScreens/ContactScreen/contact_screen.dart
+++ lib/Features/DrawerScreens/ContactScreen/contact_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shoe_fantastic/Features/DrawerScreens/ContactScreen/widgets/banner.dart';
 
 class ContactScreen extends StatelessWidget {
   const ContactScreen({super.key});
@@ -8,6 +10,10 @@ class ContactScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        scrolledUnderElevation: 0.0,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
         title: Text("Contact Us",
             style: GoogleFonts.publicSans(
               fontWeight: FontWeight.w600,
@@ -20,6 +26,20 @@ class ContactScreen extends StatelessWidget {
               Icons.arrow_back_ios_new,
               size: 19,
             )),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+        child: Column(
+          spacing: 20,
+          children: [
+            ContactTextField(
+              bannerName: "Name",
+            ),
+            ContactTextField(
+              bannerName: "Email",
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/Features/DrawerScreens/ContactScreen/widgets/banner.dart
+++ lib/Features/DrawerScreens/ContactScreen/widgets/banner.dart
@@ -1,10 +1,40 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shoe_fantastic/Ui%20Helper/Color%20Palate/color_palate.dart';
 
 class ContactTextField extends StatelessWidget {
-  const ContactTextField({super.key});
-
+  const ContactTextField({super.key, required this.bannerName});
+  final String bannerName;
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      spacing: 5,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          bannerName,
+          style: GoogleFonts.publicSans(
+            fontWeight: FontWeight.w600,
+            fontSize: 16,
+            color: Palate.blackColor,
+          ),
+        ),
+        Container(
+          height: 50,
+          width: double.infinity,
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
+          decoration: BoxDecoration(
+            color: Colors.grey.shade200,
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: TextFormField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
