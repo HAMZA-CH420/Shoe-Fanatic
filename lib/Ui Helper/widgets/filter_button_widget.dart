c3ef55cdbb0e29624abe206232c469466edf25e1
HAMZA-CH <hakram554r@gmail.com>
2025-04-16 08:59:41 +0500

    minor changes

--- lib/Ui Helper/widgets/filter_button_widget.dart
+++ lib/Ui Helper/widgets/filter_button_widget.dart
@@ -3,8 +3,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:shoe_fantastic/Ui%20Helper/Color%20Palate/color_palate.dart';
 
 class FilterButtonWidget extends StatefulWidget {
-  const FilterButtonWidget({super.key});
-
+  const FilterButtonWidget({super.key, required this.banner});
+  final String banner;
   @override
   State<FilterButtonWidget> createState() => _FilterButtonWidgetState();
 }
@@ -21,17 +21,18 @@ class _FilterButtonWidgetState extends State<FilterButtonWidget> {
         });
       },
       child: Container(
+        margin: const EdgeInsets.only(top: 8),
         height: size.height / 21,
-        width: size.width / 3.3,
+        width: size.width / 3.4,
         alignment: Alignment.center,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
+          borderRadius: BorderRadius.circular(15),
           color: isClicked ? Palate.primaryColor : Colors.grey.shade100,
         ),
         child: Text(
-          "More",
+          widget.banner,
           style: GoogleFonts.publicSans(
-            fontSize: 16,
+            fontSize: 15,
             fontWeight: FontWeight.w500,
             color: isClicked ? Colors.white : Colors.black,
           ),
--- lib/Ui Helper/widgets/filter_widget.dart
+++ lib/Ui Helper/widgets/filter_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shoe_fantastic/Ui%20Helper/Color%20Palate/color_palate.dart';
 import 'package:shoe_fantastic/Ui%20Helper/widgets/filter_banner_widget.dart';
 import 'package:shoe_fantastic/Ui%20Helper/widgets/filter_button_widget.dart';
 
@@ -14,11 +16,42 @@ Future<void> filterWidget(BuildContext context) async {
           borderRadius: BorderRadius.circular(20),
           color: Colors.white,
         ),
-        child: Column(
-          children: [
-            FilterBannerWidget(),
-            FilterButtonWidget(),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Column(
+            spacing: 12,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Filter",
+                  style: GoogleFonts.publicSans(
+                      fontWeight: FontWeight.w600,
+                      color: Palate.primaryColor,
+                      fontSize: 18),
+                ),
+              ),
+              FilterBannerWidget(),
+              Wrap(
+                spacing: 8,
+                children: [
+                  FilterButtonWidget(
+                    banner: "Men",
+                  ),
+                  FilterButtonWidget(
+                    banner: "Women",
+                  ),
+                  FilterButtonWidget(
+                    banner: "Children",
+                  ),
+                  FilterButtonWidget(
+                    banner: "Toddlers",
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       );
     },
