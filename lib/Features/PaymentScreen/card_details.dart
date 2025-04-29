ea100932fa8cfd0cf410102a6ede1c1e999cc048
HAMZA-CH <hakram554r@gmail.com>
2025-04-29 22:55:56 +0500

    minor changes

--- lib/Features/PaymentScreen/card_details.dart
+++ lib/Features/PaymentScreen/card_details.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shoe_fantastic/Features/PaymentScreen/Widgets/card_info_field.dart';
+import 'package:shoe_fantastic/Ui%20Helper/Color%20Palate/color_palate.dart';
 
 import 'Widgets/card_info_input_field.dart';
 
@@ -22,25 +23,46 @@ class CardDetails extends StatelessWidget {
               size: 20,
             )),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Column(
-          spacing: 20,
-          children: [
-            SvgPicture.asset("assets/images/credit.svg"),
-            const SizedBox(
-              height: 20,
-            ),
-            CardInfoField(hintName: "Name"),
-            CardInfoField(hintName: "Enter card number here"),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CardInfoInputField(hintName: "Expiry Date"),
-                CardInfoInputField(hintName: "CVV"),
-              ],
-            ),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                spacing: 20,
+                children: [
+                  SvgPicture.asset("assets/images/credit.svg"),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  CardInfoField(hintName: "Name"),
+                  CardInfoField(hintName: "Enter card number here"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CardInfoInputField(hintName: "Expiry Date"),
+                      CardInfoInputField(hintName: "CVV"),
+                    ],
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: Size(
+                        MediaQuery.sizeOf(context).width,
+                        MediaQuery.sizeOf(context).height / 18,
+                      ),
+                      backgroundColor: Palate.primaryColor),
+                  onPressed: () {},
+                  child: Text(
+                    "Pay",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ))
+            ],
+          ),
         ),
       ),
     );
