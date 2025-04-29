c3cc1ebb072e089f62e05e3ab6a7ca94306e2d8e
HAMZA-CH <hakram554r@gmail.com>
2025-04-29 22:44:15 +0500

    minor changes

--- lib/Features/PaymentScreen/Widgets/card_info_field.dart
+++ lib/Features/PaymentScreen/Widgets/card_info_field.dart
@@ -24,3 +24,28 @@ class CardInfoField extends StatelessWidget {
     );
   }
 }
+
+class CardInfoInputField extends StatelessWidget {
+  const CardInfoInputField({super.key, required this.hintName});
+  final String hintName;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: MediaQuery.sizeOf(context).width / 1.8,
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: hintName,
+          hintStyle: TextStyle(
+            color: Colors.grey.shade300,
+          ),
+        ),
+      ),
+    );
+  }
+}
