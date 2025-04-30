d8037e80a6b3944df6c8ccf69778b320da73a111
HAMZA-CH <hakram554r@gmail.com>
2025-04-30 09:08:18 +0500

    minor changes

--- lib/Features/DrawerScreens/NotificationScreen/widgets/notification_widget.dart
+++ lib/Features/DrawerScreens/NotificationScreen/widgets/notification_widget.dart
@@ -55,7 +55,7 @@ class NotificationWidget extends StatelessWidget {
         ),
         isNew
             ? Positioned(
-                right: 1,
+                right: -1.5,
                 child: Icon(
                   Icons.circle,
                   color: Palate.primaryColor,
