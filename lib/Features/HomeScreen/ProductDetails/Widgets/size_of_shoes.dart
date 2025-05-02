72e91d55ca4b60bbd617de0e23c1e351c9403183
HAMZA-CH <hakram554r@gmail.com>
2025-05-02 20:10:48 +0500

    minor changes

--- lib/Features/HomeScreen/ProductDetails/Widgets/size_of_shoes.dart
+++ lib/Features/HomeScreen/ProductDetails/Widgets/size_of_shoes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:shoe_fantastic/Ui%20Helper/widgets/Filter/filter_button_widget.dart';
+import 'package:shoe_fantastic/Ui%20Helper/Color%20Palate/color_palate.dart';
 
 class SizeOfShoes extends StatelessWidget {
   const SizeOfShoes({super.key});
@@ -18,12 +18,52 @@ class SizeOfShoes extends StatelessWidget {
         Wrap(
           spacing: 4,
           children: [
-            FilterButtonWidget(banner: "6"),
-            FilterButtonWidget(banner: "7"),
-            FilterButtonWidget(banner: "8"),
+            SizeOfShoe(banner: "6"),
+            SizeOfShoe(banner: "7"),
+            SizeOfShoe(banner: "8"),
           ],
         )
       ],
+    );
+  }
+}
+
+class SizeOfShoe extends StatefulWidget {
+  const SizeOfShoe({super.key, required this.banner});
+  final String banner;
+  @override
+  State<SizeOfShoe> createState() => _SizeOfShoe();
+}
+
+class _SizeOfShoe extends State<SizeOfShoe> {
+  bool isClicked = false;
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isClicked = !isClicked;
+        });
+      },
+      child: Container(
+        margin: const EdgeInsets.only(top: 8),
+        height: size.height / 21,
+        width: size.width / 4,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(22),
+          color: isClicked ? Palate.primaryColor : Colors.grey.shade100,
+        ),
+        child: Text(
+          widget.banner,
+          style: GoogleFonts.publicSans(
+            fontSize: 15,
+            fontWeight: FontWeight.w500,
+            color: isClicked ? Colors.white : Colors.black,
+          ),
+        ),
+      ),
     );
   }
 }
