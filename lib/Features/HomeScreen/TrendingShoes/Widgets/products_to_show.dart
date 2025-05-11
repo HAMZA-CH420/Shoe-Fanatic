7b16a1174ce2a9d44455c8f0e00daa3ac6b4d513
HAMZA-CH <hakram554r@gmail.com>
2025-05-11 09:03:06 +0500

    minor changes

--- lib/Features/HomeScreen/TrendingShoes/Widgets/products_to_show.dart
+++ lib/Features/HomeScreen/TrendingShoes/Widgets/products_to_show.dart
@@ -1,55 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shoe_fantastic/Features/HomeScreen/Widgets/fav_button.dart';
+import 'package:shoe_fantastic/Ui%20Helper/Color%20Palate/color_palate.dart';
 
 class ProductsToShow extends StatelessWidget {
   const ProductsToShow({super.key});
-
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
+    return Wrap(
+      children: [
+        widget(size, "myShoes.png", "Nike", "Nike Dunk Low SB X Sting-water",
+            "\$240"),
+        widget(size, "myShoes.png", "Nike", "Nike Dunk Low SB X Sting-water",
+            "\$240"),
+        widget(size, "myShoes.png", "Nike", "Nike Dunk Low SB X Sting-water",
+            "\$240"),
+        widget(size, "myShoes.png", "Nike", "Nike Dunk Low SB X Sting-water",
+            "\$240")
+      ],
+    );
+  }
+
+  ///Widget for recent shoes
+  Widget widget(
+      var size, String image, String brand, String model, String price) {
     return Container(
-      height: size.height / 3.9,
-      width: size.width / 2.3,
-      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 15),
+      margin: const EdgeInsets.only(right: 12),
+      height: size.height / 3.5,
+      width: size.width / 2.4,
       decoration: BoxDecoration(
-        color: Colors.blue,
         borderRadius: BorderRadius.circular(30),
+        color: Colors.white,
       ),
       child: Column(
-        spacing: 15,
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Align(
-            alignment: Alignment.center,
-            child: Image.asset(
-              "assets/images/myShoes.png",
-              width: size.width / 3.5,
+          SizedBox(
+            height: size.height / 6.5,
+            width: size.width / 2.4,
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 10,
+                  left: 10,
+                  child: Image.asset(
+                    "assets/images/$image",
+                    width: size.width / 3,
+                  ),
+                ),
+                Positioned(
+                    top: 8,
+                    right: 12,
+                    child: FavButton(
+                      isRecommended: false,
+                    ))
+              ],
             ),
           ),
-          Column(
-            children: [
-              Text(
-                "Jordan",
-                style: GoogleFonts.publicSans(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 13,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            child: Column(
+              spacing: 5,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  brand,
+                  style: GoogleFonts.publicSans(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-              Text(
-                "Air Force Shoe Sneakers",
-                style: GoogleFonts.publicSans(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 13,
+                Text(
+                  model,
+                  style: GoogleFonts.publicSans(
+                      fontWeight: FontWeight.w400, fontSize: 13),
                 ),
-              ),
-              Text(
-                "Jordan",
-                style: GoogleFonts.publicSans(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 13,
-                ),
-              ),
-            ],
+                Text(
+                  price,
+                  style: GoogleFonts.publicSans(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: Palate.primaryColor,
+                  ),
+                )
+              ],
+            ),
           ),
         ],
       ),
