280e6f3f398f3f644fc82df65e81e97b360b2bae
HAMZA-CH <hakram554r@gmail.com>
2025-06-01 23:26:41 +0500

    minor changes

--- lib/Services/Notification Services/notification_services.dart
+++ lib/Services/Notification Services/notification_services.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class NotificationServices {
+  //Ask for permission
   void notificationPermission() async {
     var status = await Permission.notification.status;
     if (status.isDenied) {
